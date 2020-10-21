8faa4f7187d28ea3367dbd9b39dc15bece3cd4c4
Gabor Toth <gabor.toth@live.com>
2020-10-21 22:25:42 +0200

    Initial implementation of Play

--- ChezzLib/Play.cs
+++ ChezzLib/Play.cs
@@ -4,8 +4,27 @@ using System.Text;
 
 namespace ChezzLib
 {
+    /// <summary>
+    /// Record moves on a Table
+    /// </summary>
     public class Play
     {
+        public Table Table { get; }
         public List<Move> Moves { get; set; }
+
+        public Play()
+        {
+            Table = new Table(8, 8);
+        }
+        public Play (Table Table)
+        {
+            this.Table = Table;
+        }
+
+        public void Move(Move move)
+        {
+            Table.Move(move);
+            Moves.Add(move);
+        }
     }
 }
