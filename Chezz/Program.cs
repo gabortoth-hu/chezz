48c342518782746197aaee36fceb835148aa5dc5
Gabor Toth <gabor.toth@live.com>
2020-11-10 16:52:48 +0100

    Record dummy pgn

--- Chezz/Program.cs
+++ Chezz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using ChezzLib;
 using ChezzLib.Pieces;
 
@@ -16,14 +17,17 @@ namespace Chezz
             Console.Write(table.ToString());
             Console.ReadLine();
 
+            StringBuilder pgn = new StringBuilder();
+            int i = 1;
+
             while (table.CanMove(PieceColor.White) && table.CanMove(PieceColor.Black))
             {
                 Console.WriteLine("Thinking...");
-                var whiteMove = GameEngine.NextMove(table, PieceColor.White, 5);
+                var whiteMove = GameEngine.NextMove(table, PieceColor.White, 4);
                 if (whiteMove != null)
                 {
                     Console.WriteLine("White: " + whiteMove.ToString());
-                    table.Move(whiteMove);
+                    table.Move(whiteMove); pgn.Append(i.ToString() + ". "); pgn.Append(whiteMove.ToString() + " "); 
                     Console.Write(table.ToString());
 
                     //var blackMove = GameEngine.NextMove(table, PieceColor.Black, 2);
@@ -31,16 +35,17 @@ namespace Chezz
                     if (blackMove != null)
                     {
                         Console.WriteLine("Black: " + blackMove.ToString());
-                        table.Move(blackMove);
+                        table.Move(blackMove); pgn.Append(blackMove.ToString() + " "); i++;
                         Console.Write(table.ToString());
                     }
                 }
                 
-                Console.ReadLine();
+                //Console.ReadLine();
             }
 
+            
             Console.WriteLine("*** End of game ***");
-            Console.ReadLine();
+            Console.WriteLine(pgn.ToString());
 
         }
     }
